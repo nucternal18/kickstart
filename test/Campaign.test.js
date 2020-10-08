7c723db79ff0c8eeb6beaa7b84fe3295bd92da43
“nucternal18” <aolausoro@gmail.com>
2020-10-08 19:39:16 +0100

    created contract using solidity and setup tsting using mocha

---
+++ test/Campaign.test.js
@@ -0,0 +1,58 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require("../ethereum/build/CampaignFactory.json");
+const compiledCampaign = require("../ethereum/build/Campaign.json");
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({
+      data: compiledFactory.bytecode,
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+  await factory.methods.createCampaign("100").send({
+    from: accounts[0],
+    gas: "1000000",
+  });
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
+});
+
+describe("Campaigns",() => {
+  it("deploys a factory and a campaign", async () => {
+    await assert.ok(factory.options.address);
+    await assert.ok(campaign.options.address);
+  });
+
+  it('Marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[ 0 ], manager);
+  });
+
+  it('Allows people to con tribute money and marks them as approvers', async () => {
+    await campaign.methods.contribute().send({
+      value: '200',
+      from: accounts[1]
+    })
+    const isContributer = await campaign.methods.approvers(accounts[ 1 ]).call();
+    assert(isContributer);
+  })
+});
